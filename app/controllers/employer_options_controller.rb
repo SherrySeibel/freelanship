3fa87996c1d62b08db89baf7eccb6686cff81512
Sherry Seibel <sherry.seibel@gmail.com>
2014-11-22 12:15:35 -0500

    Create a user admin landing page

---
+++ app/controllers/employer_options_controller.rb
@@ -0,0 +1,4 @@
+class EmployerOptionsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,8 @@ Rails.application.routes.draw do
   constraints Monban::Constraints::SignedIn.new do
     get "/", to: "dashboards#show", as: :dashboards
     resources :users, only: [:show]
+    resource :employer_options, only: [:show]
   end
-
-  resources :listings, only: [:new]
 
   root to: "sessions#new", via: :get
 end
