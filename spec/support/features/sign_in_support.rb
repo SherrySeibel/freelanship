45f6457f0cbf1558c85fc461c6eee8428f178361
Sherry Seibel <sherry.seibel@gmail.com>
2014-11-22 00:44:11 -0500

    Create a user authentication system
    A user can sign-in, sign-up, and sign-out

---
+++ spec/support/features/sign_in_support.rb
@@ -0,0 +1,16 @@
+module SignInSupport
+  def sign_in(user)
+    visit root_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_on "Sign in"
+  end
+
+  def when_signed_in
+    user = create(:user)
+
+    sign_in(user)
+
+    visit dashboards_path
+  end
+end
